6d1abfb4be1958a7cdfdff375e1b034739396372
Izaelle <izaelleavrillavigne@gmail.com>
2024-09-02 16:36:15 -0300

    feat:init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import styles from './App.module.css'
+import { Header } from "./components/Header"
+
+function App() {
+
+
+  return (
+    <>
+    <div className={styles.container}>
+
+    <Header/>
+    </div>
+    
+    </>
+  )
+}
+
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import styles from './Header.module.css'
+import logo from '../assets/logo.svg'
+
+export function Header(){
+    return(
+        <>
+        <header className={styles.header}>
+            <img src={logo} alt="" />
+            <div className={styles.nav}>
+                <a href="#">Sobre Nós</a>
+                <a href="#">Serviços</a>
+                <a href="#">Localização</a>
+
+            </div>
+           <button className={'button'}>Fale Conosco</button>
+        </header>
+        
+        </>
+    )
+}
