85033ca238d59f46bccb999d6b659c10b8cd37df
Izaelle <izaelleavrillavigne@gmail.com>
2024-09-03 17:16:05 -0300

    animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,42 @@
-import styles from './App.module.css'
-import { Header } from "./components/Header"
-import { SectionHome } from './components/SectionHome'
+import styles from "./App.module.css";
+import { Card } from "./components/Card";
+import { Header } from "./components/Header";
+import { SectionHome } from "./components/SectionHome";
 
 function App() {
-
-
   return (
     <>
-    <div className={styles.container}>
+      <div className={styles.container}>
+        <Header />
+        <SectionHome />
+        <div className={styles.cardsContainer}>
+        <Card
+          title={"Abertura e Regularização de Empresas"}
+          iconPath={"/cash.svg"}
+          text={
+            "Tenha toda a orientação para abrir sua empresa e deixá-la regular perante os órgãos competentes. Entre em contato agora mesmo, nós cuidaremos de todos os processos de forma integra e eficiente."
+          }
+        />
+          <Card
+          title={"Pessoas que precisam declarar imposto de renda."}
+          iconPath={"/papper.svg"}
+          text={
+            "Obrigatório para você que faz uso do carnê-leão, trabalha CLT em mais de uma entidade, tem imposto retido na fonte ou qualquer outra obrigação para com a Receita federal do Brasil"
+          }
+        />
+          <Card
+          title={"Contabilidade MEI e Simples Nacional"}
+          iconPath={"/coin.svg"}
+          text={
+            "Queremos ser o seu parceiro de confiança no que se trata de gestão contábil. A nossa equipe especializada vai lhe ajudar a alcançar o sucesso financeiro e simplificar a sua rotina contábil."
+          }
+        />
 
-    <Header/>
-    <SectionHome/>
-    </div>
-    
+        </div>
+   
+      </div>
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import styles from "./Card.module.css";
+
+export function Card({ title, iconPath, text }) {
+  return (
+    <>
+
+    <div className={styles.card}>
+        <div className={styles.iconPath}>
+          <img src={iconPath} alt="" />
+        </div>
+        <div className={styles.title}>{title}</div>
+
+        <p className={styles.text}>{text}</p>
+      </div>
+
+    
+     
+    </>
+  );
+}
