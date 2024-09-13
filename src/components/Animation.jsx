e82911aef598f3604fefce14a5fc3a505278404f
Izaelle Lavigne <izaelleavrillavigne@gmail.com>
2024-09-13 14:05:41 -0300

    ajuste no offset animation

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -4,7 +4,7 @@ export function Animation({ children, animation = "fadeIn" }) {
     <>
       <ScrollAnimation
         animateIn={animation}
-        offset={150}
+        offset={130}
         animateOnce={true}
         animatePreScroll={false}
       >
