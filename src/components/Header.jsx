0d6945261eed502dcd1d742a4565889f81082f6c
Izaelle Lavigne <izaelleavrillavigne@gmail.com>
2024-09-13 11:26:59 -0300

    feat button - img home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,21 @@ import styles from './Header.module.css'
 import logo from '../assets/logo.svg'
 
 export function Header(){
-    return(
-        <>
+    return (
+      <>
         <header className={styles.header}>
-            <img src={logo} alt="" />
-            <div className={styles.nav}>
-                <a href="#AboutUs">Sobre Nós</a>
-                <a href="#Service">Serviços</a>
-                <a href="#Location">Localização</a>
-
-            </div>
-           <button className={'button sm-hidden'}>Fale Conosco</button>
+          <img src={logo} alt="" />
+          <div className={styles.nav}>
+            <a href="#AboutUs">Sobre Nós</a>
+            <a href="#Service">Serviços</a>
+            <a href="#Location">Localização</a>
+          </div>
+          <button className={"button sm-hidden"}>
+            <a href="https://wa.link/nu8o1o"
+            target='_blank'
+            >Fale Conosco</a>
+          </button>
         </header>
-        
-        </>
+      </>
     )
 }
