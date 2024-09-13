51c61371cd5d57237fc3bc532eb24b590db8c6ce
Izaelle Lavigne <izaelleavrillavigne@gmail.com>
2024-09-13 14:12:44 -0300

    add link phone number

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,15 +21,25 @@ export function Footer() {
             <div className={styles.headline}>
               <div className={styles.address}>
                 <p>
-                  <a href="https://wa.link/nu8o1o">{CONFIG.address}</a>
+                  <a href="https://www.google.com/maps/place/Norte+Contabilidade/@-3.0485635,-59.9681326,17z/data=!3m1!4b1!4m6!3m5!1s0x926c1bc783e2ad2f:0x6fd98a7ef62b4a60!8m2!3d-3.0485635!4d-59.9655577!16s%2Fg%2F11gd1txfj7?entry=ttu&g_ep=EgoyMDI0MDkxMS4wIKXMDSoASAFQAw%3D%3D">
+                    {CONFIG.address}
+                  </a>
                 </p>
               </div>
               <div className={styles.number}>
                 <img className={styles.iconWapp} src={iconWapp} alt="" />
-                <a href={`tel:${formatNumber(CONFIG.phone1)}`}>
+                <a
+                  href={`tel:${formatNumber(
+                    CONFIG.phone1
+                  )} https://wa.link/nu8o1o`}
+                >
                   {CONFIG.phone1}
                 </a>
-                <a href={`tel:${formatNumber(CONFIG.phone2)}`}>
+                <a
+                  href={`tel:${formatNumber(
+                    CONFIG.phone2
+                  )} https://wa.link/984ee9`}
+                >
                   {CONFIG.phone2}
                 </a>
                 {/* <a href={`mailto:${CONFIG.email}`}>{CONFIG.email}</a> */}
--- src/static/config.js
+++ src/static/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  address:"Av Coronoel Sávio Belota, 59 núcleo 16, Cidade Nova, bairro Novo Aleixo - Manaus AM",
+  address:"Av Coronel Sávio Belota, 59 núcleo 16, Cidade Nova, bairro Novo Aleixo - Manaus AM",
   email: "nortecontabilidade1gmail.com",
   phone1: "(92) 9 9441-3003",
   phone2: "(92) 9 8198-6317",
